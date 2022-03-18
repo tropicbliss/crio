953c857cc1ec8b634feb86feaafc3f7973fa6132
tropicbliss <tropicbliss@protonmail.com>
2022-03-18 23:57:55 +0800

    Add examples to doc

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,39 @@
 //! Note that this is not an embedded database and there are other libraries which are better suited
 //! for this task, such as `sled`:
 //! https://github.com/spacejam/sled
+//!
+//! # Examples
+//!
+//! ```
+//! #[derive(Serialize, Deserialize, Debug)]
+//! struct Message {
+//!     id: usize,
+//!     message: String,
+//! }
+//!
+//! let msg1 = Message {
+//!     id: 1,
+//!     message: "Hello there, you suck".to_string(),
+//! };
+//! let msg2 = Message {
+//!     id: 2,
+//!     message: "No you".to_string(),
+//! };
+//! let msg3 = Message {
+//!     id: 3,
+//!     message: "You both suck".to_string(),
+//! };
+//! let messages = vec![msg1, msg2, msg3];
+//! let path = PathBuf::new("messages.pdc");
+//! let client: Client<Message> = Client::new(path)?; // If no file is found, a new empty file is created
+//! client.write(messages)?; // If no file is found, a new file is created and then written to
+//! let messages = client.load()?;
+//! if let Some(data) = messages {
+//!     println!("Here are your messages: {:?}", data);
+//! } else {
+//!     panic!("File is empty");
+//! }
+//! ```
 
 #![deny(missing_docs)]
 
